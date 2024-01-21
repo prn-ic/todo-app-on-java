447747b1e3fb72638bbce0af1504f2b5aa77883a
prn-ic <domarlaid2@mail.ru>
2024-01-21 22:06:52 +0500

    added method into interface

--- src/TodoApp/src/main/java/ru/todoapp/domain/contracts/TaskService.java
+++ src/TodoApp/src/main/java/ru/todoapp/domain/contracts/TaskService.java
@@ -2,9 +2,11 @@ package ru.todoapp.domain.contracts;
 
 import ru.todoapp.domain.entities.Task;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TaskService {
+    List<Task> getAll();
     Task getById(UUID id);
     boolean create(Task task);
     boolean update(Task task);
